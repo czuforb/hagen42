2392cbfcbf7bc21d880f2ce461e80096db19d7a4
Bence Czufor <czuforbence@gmail.com>
2021-07-02 10:10:03 +0200

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import emotionReset from "emotion-reset";
+import { Global, css } from "@emotion/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          ${emotionReset}
+          @font-face {
+            font-family: "NHaasGrotesk";
+            src: URL("NHaasGroteskDSPro-55Rg.ttf") format("truetype");
+          }
+
+          *,
+          *::after,
+          *::before {
+            box-sizing: border-box;
+            -moz-osx-font-smoothing: grayscale;
+            -webkit-font-smoothing: antialiased;
+            font-smoothing: antialiased;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
